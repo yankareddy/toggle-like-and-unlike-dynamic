dbb3694d74cf9a9754f0d15994f032d27b15d207
yankareddy <reddy.a111friend@gmail.com>
2023-12-28 19:45:07 +0530

    toggle-like-and-unlike-dynamic

---
+++ Toggle Like and Unlike.js
@@ -0,0 +1,17 @@
+let counter = 0;
+
+
+function onClickLikeButton() {
+    counter = counter + 1;
+    if (counter % 2 === 0) {
+        document.getElementById("puppyImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/white-puppy-img.png";
+        document.getElementById("likeIcon").style.color = "#cbd2d9";
+        document.getElementById("likeButton").style.backgroundColor = "#cbd2d9";
+        document.getElementById("likeButton").style.color = "#9aa5b1";
+    } else {
+        document.getElementById("puppyImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/white-puppy-liked-img.png";
+        document.getElementById("likeIcon").style.color = "#0967d2";
+        document.getElementById("likeButton").style.backgroundColor = "#0967d2";
+        document.getElementById("likeButton").style.color = "white";
+    }
+}
